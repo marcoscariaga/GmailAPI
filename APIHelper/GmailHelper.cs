596a5d44c105b09c260bfe438d0acfa1685769d8
Marcos Costa <90783203+marcoscariaga@users.noreply.github.com>
2023-09-15 16:46:53 -0300

    Replacing file

--- APIHelper/GmailHelper.cs
+++ APIHelper/GmailHelper.cs
@@ -1,12 +1,15 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace GmailAPI.APIHelper
 {
@@ -20,11 +23,57 @@ namespace GmailAPI.APIHelper
             UserCredential credential;
             using (FileStream stream = new FileStream(Convert.ToString(ConfigurationManager.AppSettings["ClientInfo"]), FileMode.Open, FileAccess.Read))
             {
+                String FolderPath = Convert.ToString(ConfigurationManager.AppSettings["CredentialsInfo"]);
+                String FilePath = Path.Combine(FolderPath, "APITokenCredentials");
 
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.FromStream(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(FilePath, true)).Result;
             }
 
-            return null;
+            GmailService service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            return service;
         }
 
+        public static void MsgMarkAsread(string hostEmailAddress, string msgId)
+        {
+            //MESSAGE MARKS AS READ AFTER READING MESSAGE
+            ModifyMessageRequest mods = new ModifyMessageRequest();
+            mods.AddLabelIds = null;
+            mods.RemoveLabelIds = new List<string> { "UNREAD" };
+            GetService().Users.Messages.Modify(mods, hostEmailAddress, msgId).Execute();
+        }
+
+        public static List<string> GetAttachments(string userId, string messageId, String outputDir)
+        {
+            try
+            {
+                List<string> fileName = new List<string>();
+                GmailService gService = GetService();
+                Message message = gService.Users.Messages.Get(userId, messageId).Execute();
+                IList<MessagePart> parts = message.Payload.Parts;
+
+                foreach(MessagePart part in parts)
+                {
+                    if (!String.IsNullOrEmpty(part.Filename))
+                    {
+                        string attId = part.Body.AttachmentId;
+                        MessagePartBody attachPart = gService.Users.Messages.Attachments.Get(userId, messageId, attId).Execute();
+
+                        byte[] data = Base64ToByte(attachPart.Data);
+                        File.WriteAllBytes(Path.Combine(outputDir, part.Filename), data);
+                        fileName.Add(part.Filename);
+                    }
+                }
+            }
+        }
     }
 }
