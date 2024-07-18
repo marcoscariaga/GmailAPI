1001d193818bef9e700847bfd4d26a6b3968b4f1
Marcos Costa <marcoscariaga@hotmail.com>
2024-07-18 11:25:17 -0300

    Changing the user info

---
+++ GmailDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GmailAPI
+{
+    public class GmailDBContext : DbContext
+    {
+        public DbSet<Info> _dbContext { get; set; }
+
+        public GmailDBContext() : base("Data Source=10.5.90.31;User ID=sa;Password=********;Initial Catalog=CONVISA_MAIL;Integrated Security=false;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;")
+        {
+        }  
+    }
+}
---
+++ Info.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GmailAPI
+{
+    public class Info
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NomeCracha { get; set; }
+        public string SexoGenero { get; set; }
+        public string RacaCorEtnia { get; set; }
+        public string Instituicao { get; set; }
+        public string Documento { get; set; }
+        public string NumeroDocumento { get; set; }
+        public string Profissao { get; set; }
+        public string Endereco { get; set; }
+        public string Bairro { get; set; }
+        public string Cep { get; set; }
+        public string Municipio { get; set; }
+        public string Cidade { get; set; }
+        public string Pais { get; set; }
+        public string NumeroCelular { get; set; }
+        public string Email { get; set; }
+        public string TemDeficiencia { get; set; }
+        public string DescricaoDeficiencia { get; set; }
+        public string ComAcompanhante { get; set; }
+        public string PrecisaRecursos { get; set; }
+        public string DescricaoRecurso { get; set; }
+        public string Categoria { get; set; }
+        public string Data { get; set; }
+
+        public Info() { }
+    }
+}
